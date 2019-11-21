f0c494d02e628c0e2a967d129fc5799e4a7d3aba
lys_mage <lys_mage@126.com>
2019-11-21 23:34:37 +0800

    commit test

---
+++ src/com/lys/leetcode/LRUCache.java
@@ -0,0 +1,25 @@
+package com.lys.leetcode;
+
+
+import java.util.LinkedHashMap;
+
+class LRUCache {
+    LinkedHashMap map = new LinkedHashMap();
+    public LRUCache(int capacity) {
+    }
+
+    public int get(int key) {
+        return 0;
+    }
+
+    public void put(int key, int value) {
+
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
